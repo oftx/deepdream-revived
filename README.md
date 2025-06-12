83dfb622de69e44262a3a08f9812269e392819eb
ChameleonAI <ChamelAIon@proton.me>
2025-06-12 09:39:04 -0500

    Added octaves mode

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # deepdream-revived
-A version of DeepDream that works in modern-day python
+A version of DeepDream that works in (relatively) modern-day python.
+
+The original [DeepDream](https://github.com/google/deepdream) and related projects like [DeepDreamVideo](https://github.com/graphific/DeepDreamVideo) were written 10 years ago in python 2, so they're effectively dead for anyone interested in running it today. I've attempted to port this functionality to run in the most recent version of python 3 possible.
+
+Note that I'm not an expert with this stuff. It's simply my best effort to preserve old tech.
+
+## Installation
+
+***WARNING***: This setup is not noob friendly. You need some familiarity with python virtual environments and a willingness to troubleshoot platform-specific depencency issues. Please  read carefully as there are some important caveats to know before you try to run it.
+
+The most recent version of python that works for me is **python 3.10**. The main dependency that hinders newer python versions is [tensorflow](https://www.tensorflow.org/install). Supposedly tensorflow works for python 3.8-3.11, but I personally didn't have success with 3.11.
+
+To get python 3.10, you may have to compile it from source, find a legacy installer, or find an unofficial package, such as the [deadsnakes](https://linuxcapable.com/how-to-install-python-3-10-on-ubuntu-linux/) repository for Ubuntu or [python310](https://aur.archlinux.org/packages/python310) from the Arch Linux AUR.
+
+
+- If you're on **Windows+Nvidia**, note that you'll need to install the [Microsoft Visual C++ redistributable](https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170)
+- If you're on **Windows+AMD**, you may be able to get it to work through [WSL](https://wiki.archlinux.org/title/Install_Arch_Linux_on_WSL)
+- If you're on **Linux+AMD** (like me), you'll have to install [tensorflow-rocm](https://rocm.docs.amd.com/projects/install-on-linux/en/latest/install/3rd-party/tensorflow-install.html)
+- If you're on **Linux+Nvidia** it may work as-is, but you might have to select a specific version of tensorflow (see instructions below)
+
+You may be asking, why doesn't this use pytorch? My implementation is based on [this tutorial](https://www.tensorflow.org/tutorials/generative/deepdream) from tensorflow. DeepDream is very old and predates pytorch so that's why this implementation is so wonky. I don't know enough about pytorch or tensorflow to know how to port it. I welcome anyone who has ideas on how to do that, because eliminating the tensorflow dependency would make life much easier.
+
+### Installation Steps
+1. Setup a virtual environment:
+```
+python3.10 -m venv venv
+```
+2. Install tensorflow
+
+For Nvidia:
+```
+pip install tensorflow
+```
+For AMD:
+```
+pip install tensorflow-rocm
+```
+However, the tensorflow-rocm in pip is very out of date. So you may want to install a newer release:
+```
+pip install --upgrade tensorflow-rocm==2.16.2 -f https://repo.radeon.com/rocm/manylinux/rocm-rel-6.2.3/
+```
+I couldn't get anything newer to work, but feel free to try other releases by browsing [here](https://repo.radeon.com/rocm/manylinux/).\
+Despite my best efforts, I still can't get GPU inference to work, so I need `tensorflow-rocm` **and** I need to specify `--cpu` when running inference. Good luck and if you get GPU inference to work on AMD, let me know what specific configuration works for you.
+
+***NOTE***: The specific version of tensorflow that you install, and usage of CPU vs GPU, can drastically change what the output looks like. I don't know why this is the case. If you want to get results very close to the [tutorial](https://www.tensorflow.org/tutorials/generative/deepdream), use the pip version [tensorflow-rocm 2.14.0.600](https://pypi.org/project/tensorflow-rocm/2.14.0.600/) from January 2024 or [tensorflow 2.14.0](https://pypi.org/project/tensorflow/2.14.0/) from September 2023.
+
+3. Install additional requirements:
+```
+pip install -r requirements.txt
+```
+
+## Running Inference
+By default, the script is configured to run a simple example that loads `example.png` from the current working directory, so you can test your setup by simply running:
+```
+python dream.py
+```
+Outputs are put into the `output` directory.
+
+If you're having problems with GPU inference, try CPU only:
+```
+python dream.py --cpu
+```
+Specify your own image with `-i/--input`:
+```
+python dream.py -i input.jpg
+```
+
+### Command Line Flags
+- `--cpu` Disables GPU inference, running in CPU only mode
+- `--steps` Sets the number of inference steps (default is 100)
+  - In my case, using `tensorflow-rocm==2.16.2` produces similar outputs with drastically fewer steps than `2.14.0.600`, like 4-10 steps instead of 100.  
+- `--step_size` Sets the size of each step (default is 0.1)
+- `--mode` Sets the inference mode, which can be `"simple"` or `"octaves"`. Simple mode takes the least amount of time, octaves mode is more flexible and can produce different patterns depending on how you specify the octaves.
--- dream.py
+++ dream.py
@@ -1,22 +1,22 @@
+import argparse
 import os
 import traceback
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # https://stackoverflow.com/questions/38073432/how-to-suppress-verbose-tensorflow-logging
+os.environ["AUTOGRAPH_VERBOSITY"] = "0" # https://www.tensorflow.org/api_docs/python/tf/autograph/set_verbosity
 import tensorflow
 import numpy as np
-#import matplotlib as mpl
-#import IPython.display as display
 import PIL.Image
-
 from tensorflow.keras import layers, models
 
 # Download an image and read it into a NumPy array.
 def download(url, max_dim=None):
-  name = url.split('/')[-1]
-  image_path = tensorflow.keras.utils.get_file(name, origin=url)
-  img = PIL.Image.open(image_path)
-  if max_dim:
-    img.thumbnail((max_dim, max_dim))
-  return np.array(img)
+    name = url.split('/')[-1]
+    image_path = tensorflow.keras.utils.get_file(name, origin=url)
+    img = PIL.Image.open(image_path)
+    if max_dim:
+        img.thumbnail((max_dim, max_dim))
+    return np.array(img)
 
 def load_img(image_path : str, max_dim=None):
     img = PIL.Image.open(image_path)
@@ -26,88 +26,155 @@ def load_img(image_path : str, max_dim=None):
 
 # Normalize an image
 def deprocess(img):
-  img = 255*(img + 1.0)/2.0
-  return tensorflow.cast(img, tensorflow.uint8)
-
-# Display an image
-def show(img):
-  display.display(PIL.Image.fromarray(np.array(img)))
+    img = 255*(img + 1.0)/2.0
+    return tensorflow.cast(img, tensorflow.uint8)
 
 def calc_loss(img, model):
-  # Pass forward the image through the model to retrieve the activations.
-  # Converts the image into a batch of size 1.
-  img_batch = tensorflow.expand_dims(img, axis=0)
-  layer_activations = model(img_batch)
-  if len(layer_activations) == 1:
-    layer_activations = [layer_activations]
+    # Pass forward the image through the model to retrieve the activations.
+    # Converts the image into a batch of size 1.
+    img_batch = tensorflow.expand_dims(img, axis=0)
+    layer_activations = model(img_batch)
+    if len(layer_activations) == 1:
+        layer_activations = [layer_activations]
 
-  losses = []
-  for act in layer_activations:
-    loss = tensorflow.math.reduce_mean(act)
-    losses.append(loss)
+    losses = []
+    for act in layer_activations:
+        loss = tensorflow.math.reduce_mean(act)
+        losses.append(loss)
 
-  return  tensorflow.reduce_sum(losses)
+    return  tensorflow.reduce_sum(losses)
 
 class DeepDream(tensorflow.Module):
-  def __init__(self, model):
-    self.model = model
+    def __init__(self, model):
+        self.model = model
 
-  @tensorflow.function(
-      input_signature=(
-        tensorflow.TensorSpec(shape=[None,None,3], dtype=tensorflow.float32),
-        tensorflow.TensorSpec(shape=[], dtype=tensorflow.int32),
-        tensorflow.TensorSpec(shape=[], dtype=tensorflow.float32),)
-  )
-  def __call__(self, img, steps, step_size):
-      print("Tracing")
-      loss = tensorflow.constant(0.0)
-      for n in tensorflow.range(steps):
-        with tensorflow.GradientTape() as tape:
-          # This needs gradients relative to `img`
-          # `GradientTape` only watches `tensorflow.Variable`s by default
-          tape.watch(img)
-          loss = calc_loss(img, self.model)
+    @tensorflow.function(
+        input_signature=(
+            tensorflow.TensorSpec(shape=[None,None,3], dtype=tensorflow.float32),
+            tensorflow.TensorSpec(shape=[], dtype=tensorflow.int32),
+            tensorflow.TensorSpec(shape=[], dtype=tensorflow.float32),)
+    )
+    def __call__(self, img, steps, step_size):
+        loss = tensorflow.constant(0.0)
+        for n in tensorflow.range(steps):
+            with tensorflow.GradientTape() as tape:
+                # This needs gradients relative to `img`
+                # `GradientTape` only watches `tensorflow.Variable`s by default
+                tape.watch(img)
+                loss = calc_loss(img, self.model)
 
-        # Calculate the gradient of the loss with respect to the pixels of the input image.
-        gradients = tape.gradient(loss, img)
+            # Calculate the gradient of the loss with respect to the pixels of the input image.
+            gradients = tape.gradient(loss, img)
+
+            # Normalize the gradients.
+            gradients /= tensorflow.math.reduce_std(gradients) + 1e-8 
+
+            # In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
+            # You can update the image by directly adding the gradients (because they're the same shape!)
+            img = img + gradients*step_size
+            img = tensorflow.clip_by_value(img, -1, 1)
+
+        return loss, img
+
+def random_roll(img, maxroll):
+    # Randomly shift the image to avoid tiled boundaries.
+    shift = tensorflow.random.uniform(shape=[2], minval=-maxroll, maxval=maxroll, dtype=tensorflow.int32)
+    img_rolled = tensorflow.roll(img, shift=shift, axis=[0,1])
+    return shift, img_rolled
+
+class TiledGradients(tensorflow.Module):
+    def __init__(self, model):
+        self.model = model
+
+    @tensorflow.function(
+        input_signature=(
+            tensorflow.TensorSpec(shape=[None,None,3], dtype=tensorflow.float32),
+            tensorflow.TensorSpec(shape=[2], dtype=tensorflow.int32),
+            tensorflow.TensorSpec(shape=[], dtype=tensorflow.int32),)
+    )
+    def __call__(self, img, img_size, tile_size=512):
+        shift, img_rolled = random_roll(img, tile_size)
+
+        # Initialize the image gradients to zero.
+        gradients = tensorflow.zeros_like(img_rolled)
+
+        # Skip the last tile, unless there's only one tile.
+        xs = tensorflow.range(0, img_size[1], tile_size)[:-1]
+        if not tensorflow.cast(len(xs), bool):
+            xs = tensorflow.constant([0])
+        ys = tensorflow.range(0, img_size[0], tile_size)[:-1]
+        if not tensorflow.cast(len(ys), bool):
+            ys = tensorflow.constant([0])
+
+        for x in xs:
+            for y in ys:
+                # Calculate the gradients for this tile.
+                with tensorflow.GradientTape() as tape:
+                    # This needs gradients relative to `img_rolled`.
+                    # `GradientTape` only watches `tensorflow.Variable`s by default.
+                    tape.watch(img_rolled)
+
+                    # Extract a tile out of the image.
+                    img_tile = img_rolled[y:y+tile_size, x:x+tile_size]
+                    loss = calc_loss(img_tile, self.model)
+
+                # Update the image gradients for this tile.
+                gradients = gradients + tape.gradient(loss, img_rolled)
+
+        # Undo the random shift applied to the image and its gradients.
+        gradients = tensorflow.roll(gradients, shift=-shift, axis=[0,1])
 
         # Normalize the gradients.
         gradients /= tensorflow.math.reduce_std(gradients) + 1e-8 
 
-        # In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
-        # You can update the image by directly adding the gradients (because they're the same shape!)
-        img = img + gradients*step_size
-        img = tensorflow.clip_by_value(img, -1, 1)
-
-      return loss, img
-
-def run_deep_dream_simple(img, steps=100, step_size=0.01):
-  # Convert from uint8 to the range expected by the model.
-  img = tensorflow.keras.applications.inception_v3.preprocess_input(img)
-  img = tensorflow.convert_to_tensor(img)
-  step_size = tensorflow.convert_to_tensor(step_size)
-  steps_remaining = steps
-  step = 0
-  while steps_remaining:
-    if steps_remaining>100:
-      run_steps = tensorflow.constant(100)
-    else:
-      run_steps = tensorflow.constant(steps_remaining)
-    steps_remaining -= run_steps
-    step += run_steps
-
-    loss, img = deepdream(img, run_steps, tensorflow.constant(step_size))
-
-    #display.clear_output(wait=True)
-    #show(deprocess(img))
-    print ("Step {}, loss {}".format(step, loss))
+        return gradients
 
 
-  result = deprocess(img)
-  #display.clear_output(wait=True)
-  #show(result)
+def run_deep_dream_simple(img, dream_model, steps=100, step_size=0.01):
+    deepdream = DeepDream(dream_model)
+    # Convert from uint8 to the range expected by the model.
+    img = tensorflow.keras.applications.inception_v3.preprocess_input(img)
+    img = tensorflow.convert_to_tensor(img)
+    step_size = tensorflow.convert_to_tensor(step_size)
+    steps_remaining = steps
+    step = 0
+    while steps_remaining:
+        if steps_remaining>100:
+            run_steps = tensorflow.constant(100)
+        else:
+            run_steps = tensorflow.constant(steps_remaining)
+        steps_remaining -= run_steps
+        step += run_steps
 
-  return result
+        loss, img = deepdream(img, run_steps, tensorflow.constant(step_size))
+
+        print ("  Step {}, loss {}".format(step, loss))
+
+    result = deprocess(img)
+    return result
+
+def run_deep_dream_with_octaves(img, dream_model, steps_per_octave=100, step_size=0.01, octaves=range(-2,3), octave_scale=1.3):
+    get_tiled_gradients = TiledGradients(dream_model)
+    base_shape = tensorflow.shape(img)
+    img = tensorflow.keras.utils.img_to_array(img)
+    img = tensorflow.keras.applications.inception_v3.preprocess_input(img)
+
+    initial_shape = img.shape[:-1]
+    img = tensorflow.image.resize(img, initial_shape)
+    for octave in octaves:
+        print(f'  Octave {octave}')
+        # Scale the image based on the octave
+        new_size = tensorflow.cast(tensorflow.convert_to_tensor(base_shape[:-1]), tensorflow.float32)*(octave_scale**octave)
+        new_size = tensorflow.cast(new_size, tensorflow.int32)
+        img = tensorflow.image.resize(img, new_size)
+
+        for step in range(steps_per_octave):
+            gradients = get_tiled_gradients(img, new_size)
+            img = img + gradients*step_size
+            img = tensorflow.clip_by_value(img, -1, 1)
+
+    result = deprocess(img)
+    return result
 
 
 def save_img(img, img_name : str):
@@ -115,16 +182,27 @@ def save_img(img, img_name : str):
 
 if __name__ == '__main__':
     try:
-        print("Num GPUs Available: ", len(tensorflow.config.list_physical_devices('GPU')))
-        #url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg'
-        # Downsizing the image makes it easier to work with.
-        #original_img = download(url, max_dim=500)
-        #save_img(original_img, 'example.png')
-        original_img = load_img('example.png', max_dim=500)
+        parser = argparse.ArgumentParser(prog='DeepDream runner')
+        parser.add_argument('-i', '--input', type=str, default='example.png', help='Input file to process')
+        parser.add_argument('-m', '--mode', type=str, choices=['simple', 'octaves'], default='simple', help='DeepDream processing method')
+        parser.add_argument('-s', '--steps', type=int, default=100, help='Total number of steps, or steps per octave if using "octaves" mode')
+        parser.add_argument('--step_size', type=float, default=0.1, help='Step size')
+        parser.add_argument('--cpu', action='store_true', help="Use tensorflow in CPU only mode")
+
+        args, unknown_args = parser.parse_known_args()
+        if help in args:
+            parser.print_help()
+
+        if args.cpu:
+            tensorflow.config.set_visible_devices([], 'GPU')
+            print('Using CPU only mode.')
+        else:
+            print('Num GPUs Available: ', len(tensorflow.config.list_physical_devices('GPU')))
         
-        #show(original_img)
-        #display.display(display.HTML('Image cc-by: <a "href=https://commons.wikimedia.org/wiki/File:Felis_catus-cat_on_snow.jpg">Von.grzanka</a>'))
+        original_img = load_img(args.input, max_dim=768)
+
         base_model = tensorflow.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+
         # Maximize the activations of these layers
         names = ['mixed3', 'mixed5']
         layers = [base_model.get_layer(name).output for name in names]
@@ -132,8 +210,14 @@ if __name__ == '__main__':
         # Create the feature extraction model
         dream_model = tensorflow.keras.Model(inputs=base_model.input, outputs=layers)
 
-        deepdream = DeepDream(dream_model)
-        dream_img = run_deep_dream_simple(img=original_img, steps=100, step_size=0.01)
+        print(f'Using DeepDream mode "{args.mode}", steps: {args.steps}, step size: {args.step_size}')
+        if args.mode == 'simple':
+            dream_img = run_deep_dream_simple(original_img, dream_model, steps=args.steps, step_size=args.step_size)
+        elif args.mode == 'octaves':
+            dream_img = run_deep_dream_with_octaves(original_img, dream_model, steps_per_octave=args.steps, step_size=args.step_size)
+        else:
+            raise RuntimeError(f'Unrecognized mode {args.mode}')
+
         os.makedirs('output', exist_ok=True)
         save_img(dream_img, f'output/01.png')
         
